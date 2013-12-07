ef91e222adffc6190907e4d2c3f7b9488af9b56f
raivis <raivis@9a5a89c9-c25e-4995-9a45-0d45e55fc53a>
2013-12-07 23:44:54 +0000

    -Added matrix transforms for Polygon class -Added new command to path tag. l and L, for line to command -Improve m and M command in path tag, if initiated more than one, converts it self to line

--- trunk/polygon.h
+++ trunk/polygon.h
@@ -4,6 +4,8 @@
 #include <QPolygonF>
 #include <QtMath>
 #include <QDebug>
+#include "transform.h"
+#include <QTransform>
 
 class Polygon
 {
@@ -11,6 +13,8 @@ public:
     explicit Polygon(QVector<QPointF> points, bool close = true);
     QPolygonF getPolygon() { return myPolygon; }
     void setTranslate(QPointF offset);
+    void setRotation(rotationTransform transform);
+    void setMatrix(matrix3x3_struct matrix);
 
 private:
     QPolygonF myPolygon;
--- trunk/transform.h
+++ trunk/transform.h
@@ -6,17 +6,41 @@
 #include <QPointF>
 #include <QStringList>
 
+struct rotationTransform
+{
+    double angle;
+    QPointF center;
+};
+
+struct matrix3x3_struct
+{
+    double m11;
+    double m12;
+    double m13;
+    double m21;
+    double m22;
+    double m23;
+    bool isSet;
+};
+
+
 class Transform
 {
 public:
+    Transform();
     Transform(QString transform);
 
     QPointF getTranslate() { return translate; }
-    double getRotate() {return rotation; }
+    rotationTransform getRotate() {return rotation; }
+    matrix3x3_struct getMatrix() { return matrix; }
+
 
 private:
     QPointF translate;
-    double rotation;
+    rotationTransform rotation;
+    matrix3x3_struct matrix;
+
 };
 
 #endif // TRANSFORM_H
+
